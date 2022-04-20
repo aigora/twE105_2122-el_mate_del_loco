158d2b8db1ef0f8a9005636921ebadb0c2ef3dcd
inigoazqueta <inigo.azqueta@alumnos.upm.es>
2022-04-20 13:45:14 +0200

    Menu Principal
    Código de un Menu básico para el programa.

--- Codigo/MenuPrincipal.c
+++ Codigo/MenuPrincipal.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
 
+int main()
+{
+  char opcion;
+
+  printf("\n  \t\t     ¡¡¡BIENVENIDOS AL MATE DEL LOCO!!!\n\
+                 ¡EL MEJOR JUEGO DE AJEDREZ PROGRAMADO EN C!\n\
+                             ¿LISTO PARA JUGAR?\n\n\
+            \t a- Empezar Partida\n\
+            \t b- Como Jugar\n\
+            \t c- Salir del programa\n\
+\nIntroduce la letra de la opción deseada: ");
+
+ do
+ {
+    scanf(" %c",&opcion);
+    
+    switch (opcion)
+    {
+     case 'a':
+      printf("Has elegido Jugar\n");
+      break;
+
+     case 'b':
+      printf("Has elegido ver las instrucciones\n");
+      break;
+
+     case 'c':
+      printf("¡¡GRACIAS POR PROBAR EL JUEGO, ESPERAMOS VERTE PRONTO!!\n");
+      break;
+  
+     default:
+      printf("Opción no válida, vuelva a introducir un número\n");
+      break;
+    }
+
+ } while (opcion != 'c');
+
+    return 0;
+}
