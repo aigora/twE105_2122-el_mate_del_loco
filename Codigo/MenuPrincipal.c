751bbca77e26fffb1ca5cf4fee1ead3363f419fa
Javierdelahoz4 <79969759+Javierdelahoz4@users.noreply.github.com>
2022-05-13 13:15:54 +0200

    Update MenuPrincipal.c

--- Codigo/MenuPrincipal.c
+++ Codigo/MenuPrincipal.c
@@ -1,41 +1,40 @@
 #include <stdio.h>
-
-int main()
+int main ()
 {
-  char opcion;
+  FILE *fbienvenida;
+  int i,opcion;
 
-  printf("\n  \t\t     ¡¡¡BIENVENIDOS AL MATE DEL LOCO!!!\n\
-                 ¡EL MEJOR JUEGO DE AJEDREZ PROGRAMADO EN C!\n\
-                             ¿LISTO PARA JUGAR?\n\n\
-            \t a- Empezar Partida\n\
-            \t b- Como Jugar\n\
-            \t c- Salir del programa\n\
-\nIntroduce la letra de la opción deseada: ");
+  fbienvenida = fopen("Bienvenida.txt", "r");
+  if (fbienvenida == NULL) {
+    printf("Ha habido un problema, por favor intentelo de nuevo\n");
+    return -1;
+  }
+
 
  do
  {
-    scanf(" %c",&opcion);
+    scanf(" %i",&opcion);
     
     switch (opcion)
     {
-     case 'a':
-      printf("Has elegido Jugar\n");
+     case '1':
+      printf("Has elegido Jugar,¿mucha suerte!\n");
       break;
 
-     case 'b':
+     case '2':
       printf("Has elegido ver las instrucciones\n");
       break;
 
-     case 'c':
+     case '3':
       printf("¡¡GRACIAS POR PROBAR EL JUEGO, ESPERAMOS VERTE PRONTO!!\n");
       break;
   
      default:
-      printf("Opción no válida, vuelva a introducir un número\n");
-      break;
+       printf("El numero introducido no corresponde con ninguna opcion, por favor seleccione otro numero.\n");
+      
     }
 
- } while (opcion != 'c');
+ } while (opcion != '3');
 
     return 0;
 }
